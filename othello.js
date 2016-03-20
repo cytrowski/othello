e3069c97751b63250b73c61697a4d6d0fe675b66
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2016-03-20 14:18:40 +0100

    Functional othello

--- othello.js
+++ othello.js
@@ -1,22 +1,3 @@
-Array.prototype.takeUntil = function (predicate) {
-  var result = [];
-  for (var i = 0; i < this.length; i++) {
-    if (predicate(this[i]) === true) {
-      break;
-    }
-    result.push(this[i]);
-  }
-  return result;
-};
-
-Array.range = function (n) {
-  var result = [];
-  for (var i = 0; i < n; i++) {
-    result.push(i);
-  }
-  return result;
-};
-
 var players = {
   white: {
     name: 'white'
@@ -31,178 +12,132 @@ players.black.next = players.white;
 
 $(function () {
   var player = players.white,
-      $board = createBoard(8, player);
+      board = gameBoard(8, player);
 
-  $('#app')
-    .append($board)
-    .on('click', 'td', function () {
-      try {
-        putChecker(this, player, false);
-        player = player.next;
-      } catch (e) {
-        console.log(e);
-      }
+  $('#app').append($(board).on('click', 'td', function () {
+    var node = this,
+        oponentNodes;
 
-      if ($board.find('td:not(.checker)').length === 0) {
-        console.log('Game over');
-        console.log(player.name + ': ' + $board.find('.' + player.name).length);
-        console.log(player.next.name + ': ' + $board.find('.' + player.next.name).length);
-        $board.off('click');
-        return;
-      }
+    if (nodeHasChecker(node)) {
+      return console.log('This cell is occupied.');
+    }
 
-      if (
-        $('#app')
-          .find('td:not(.checker)')
-          .get()
-          .filter(function (node) {
-            return putChecker(node, player, true);
-          }).length === 0
-      ) {
-        // if no places for current player to play then switch player
-        player = player.next;
-      };
-    });
+    oponentNodes = endangeredOponentNodes(this, player);
+    if (oponentNodes.length === 0) {
+      return console.log('You cannot put ' + player.name + ' checker here!');
+    }
+
+    putCheckerOnNode(this, player);
+    replaceOponentNodes(oponentNodes, player);
+
+    if (boardIsFull(board)) {
+      return endGame(board, player);
+    }
+
+    if (playerHasValidMoves(player, board)) {
+      return player = player.next;
+    }
+
+    return console.log('Oponent does not have valid moves. ' + player.name + ' has one extra move.');
+  }));
 });
 
-function putChecker(domNode, player, checkOnly) {
-  var directions = [
-    {
-      name: 'top',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .prev()
-          .find(':eq('+ $(node).index() +')');
-      }
-    },
+function getEmptyCells(board) {
+  return $(board).find('td:not(.checker)').get();
+}
 
-    {
-      name: 'top-right',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .prev()
-          .find(':eq('+ ($(node).index() + 1) +')');
-      }
-    },
-
-    {
-      name: 'right',
-      next: function (node) {
-        return $(node)
-          .next();
-      }
-    },
-
-    {
-      name: 'bottom-right',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .next()
-          .find(':eq('+ ($(node).index() + 1) +')');
-      }
-    },
-
-    {
-      name: 'bottom',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .next()
-          .find(':eq('+ $(node).index() +')');
-      }
-    },
-
-    {
-      name: 'bottom-left',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .next()
-          .find(':eq('+ ($(node).index() - 1) +')');
-      }
-    },
-
-    {
-      name: 'left',
-      next: function (node) {
-        return $(node)
-          .prev();
-      }
-    },
-
-    {
-      name: 'top-left',
-      next: function (node) {
-        return $(node)
-          .parent()
-          .prev()
-          .find(':eq('+ ($(node).index() - 1) +')');
-      }
-    }
-  ];
-
-  if ($(this).hasClass('checker')) {
-    throw 'This field is occupied';
-  }
-
-  var oponentNodes = directions.map(function (dir) {
-    return getCheckerNodes(domNode, dir);
-  }).filter(function (checkerNodesInDirection) {
-    // pass only directions which starts with oponent node
-    return checkerNodesInDirection.length > 0 &&
-      checkerNodesInDirection[0].hasClass(player.next.name) &&
-      checkerNodesInDirection.slice(1).some(function (item) {
-        return $(item).hasClass(player.name);
-      });
-  }).reduce(function (prev, curr) {
-    return prev.concat(curr.takeUntil(function (item) {
-      return $(item).hasClass(player.name);
-    }));
-  }, []);
-
-  if (checkOnly) {
-    return oponentNodes.length > 0;
-  }
-
-  if (oponentNodes.length === 0) {
-    throw 'You cannot put a ' + player.name + ' checker here!';
-  }
-
-  oponentNodes.concat(domNode).forEach(function (node) {
-    $(node)
-      .removeClass(player.next.name)
-      .addClass(player.name)
-      .addClass('checker');
+function playerHasValidMoves(player, board) {
+  return getEmptyCells().filter(function (cell) {
+    return endangeredOponentNodes(cell, player).length > 0;
   });
 }
 
-function getCheckerNodes(node, dir) {
-  var checkerNodes = [];
-  var $nextNode = dir.next(node);
-  if ($nextNode.hasClass('checker')) {
-    checkerNodes.push($nextNode);
-    return checkerNodes.concat(getCheckerNodes($nextNode, dir));
-  }
-  return checkerNodes;
+function boardIsFull(board) {
+  return getEmptyCells(board).length === 0;
 }
 
-function createBoard(size, player) {
+function endGame(board, player) {
+  var $board = $(board);
+  console.log('Game over');
+  console.log(player.name + ': ' + $board.find('.' + player.name).length);
+  console.log(player.next.name + ': ' + $board.find('.' + player.next.name).length);
+  $board.off('click');
+}
+
+function endangeredOponentNodes(domNode, player) {
+  return [
+    function (node) {return $(node).parent().prev().find(':nth-child('+ ($(node).index() + 1) +')');}, // top
+    function (node) {return $(node).parent().prev().find(':nth-child('+ ($(node).index() + 2) +')');}, // top-right
+    function (node) {return $(node).next();}, // right
+    function (node) {return $(node).parent().next().find(':nth-child('+ ($(node).index() + 2) +')');}, // bottom-right
+    function (node) {return $(node).parent().next().find(':nth-child('+ ($(node).index() + 1) +')');}, // bottom
+    function (node) {return $(node).parent().next().find(':nth-child('+ ($(node).index() - 0) +')');}, // bottom-left
+    function (node) {return $(node).prev();}, // left
+    function (node) {return $(node).parent().prev().find(':nth-child('+ ($(node).index() - 0) +')');} // top-left
+  ].map(function (dir) {
+    return nodesWithCheckers(domNode, dir);
+  }).filter(function (checkerNodesInDirection) {
+    return checkerIsOponent(checkerNodesInDirection.head(), player) &&
+      checkersContainPlayer(checkerNodesInDirection.tail(), player);
+  }).reduce(function (prev, checkerNodes) {
+    return prev.concat(takeOponentsUntilPlayer(checkerNodes, player));
+  }, []);
+}
+
+function takeOponentsUntilPlayer(checkers, player) {
+  return checkers.takeUntil(function (item) {
+    return $(item).hasClass(player.name);
+  });
+}
+
+function checkerIsOponent(checker, player) {
+  return $(checker).hasClass(player.next.name);
+}
+
+function checkersContainPlayer(checkers, player) {
+  return checkers.some(function (checker) {
+    return $(checker).hasClass(player.name);
+  });
+}
+
+function putCheckerOnNode(node, player) {
+  var $node = $(node);
+  if ($node.hasClass('checker')) {
+    throw 'This field is occupied';
+  } else {
+    $node.addClass('checker').addClass(player.name);
+  }
+}
+
+function replaceOponentNodes(nodes, player) {
+  nodes.forEach(function (node) {
+    $(node).removeClass(player.next.name).addClass(player.name);
+  });
+};
+
+function nodeHasChecker(node) {
+  return $(node).hasClass('checker');
+}
+
+function nodesWithCheckers(node, dir) {
+  var nodes = [];
+  var nextNode = dir(node);
+  if (nodeHasChecker(nextNode)) {
+    nodes.push(nextNode);
+    return nodes.concat(nodesWithCheckers(nextNode, dir));
+  }
+  return nodes;
+}
+
+function gameBoard(size, player) {
+  function checker(player) { return player.name + ' checker'; }
   return $('<table>').append(Array.range(size).map(function (y) {
     return $('<tr>').append(Array.range(size).map(function (x) {
-      var $cell = $('<td>');
-      $cell.addClass('cell');
-      if (x === 3 && y === 3 || x === 4 && y === 4) {
-        $cell.addClass(player.name).addClass('checker');
-      }
-      if (x === 4 && y === 3 || x === 3 && y === 4) {
-        $cell.addClass(player.next.name).addClass('checker');
-      }
-      return $cell;
+      return $('<td>').addClass('cell').addClass(
+        (x === 3 && y === 3 || x === 4 && y === 4) ?
+          checker(player) : (x === 3 && y === 4 || x === 4 && y === 3) ?
+          checker(player.next) : ''
+      );
     }));
   }));
 }
-
-
